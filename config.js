14066e89a629c0bddd5bf85eb3fe18f4b8fa6792
Gister122654 <ltminh7192@gmail.com>
2026-02-07 12:01:51 +0700

    Hoan thien web

---
+++ config.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBYh55hTOBJsKwC2AE52NZZsYey-zWVSbs",
+
+  authDomain: "final2-93c05.firebaseapp.com",
+
+  projectId: "final2-93c05",
+
+  storageBucket: "final2-93c05.firebasestorage.app",
+
+  messagingSenderId: "588950176838",
+
+  appId: "1:588950176838:web:d704d8925356889d90f647",
+
+  measurementId: "G-XCSFBQ22JV"
+
+};
